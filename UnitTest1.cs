5489f42781046973f117f31a67e443a1488e52ba
Lamassu15 <bazo.assyr@gmail.com>
2024-04-10 13:24:02 +0200

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ConnectFour
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CheckForWin_NoWinner_EmptyBoard()
+        {
+            // Arrange
+            GameState state = new GameState();
+
+            // Act
+            GameState.WinState result = state.CheckForWin();
+
+            // Assert
+            Assert.Equals(GameState.WinState.No_Winner, result);
+        }
+
+        [TestMethod]
+        public void CheckForWin_TieGame()
+        {
+            // Arrange
+            var gameState = new GameState();
+            for (int i = 0; i < 42; i++)
+            {
+                gameState.TheBoard.Add(i % 2 + 1);
+            }
+
+            // Act
+            var result = gameState.CheckForWin();
+
+            // Assert
+            Assert.Equals(GameState.WinState.Tie, result);
+        }
+    }
+}
